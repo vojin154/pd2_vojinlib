1403a1f2d72319f6d0a8e668bbe8482fe065a050
vojin154 <vojin154@gmail.com>
2024-03-23 19:17:52 +0100

    Create Hooks.lua with code to create/remove update()

--- main.lua
+++ main.lua
@@ -1,12 +1,48 @@
+---@diagnostic disable: undefined-field
 if not _G.VojinLIB then
-    _G.VojinLIB = {}
+    _G.VojinLIB = _G.class()
+end
 
-    function VojinLIB:Setup()
-        self._mod_path = ModPath
-        self._save_path = SavePath
+--Caching globals
+local VojinLIB = _G.VojinLIB
+local RequiredScript = _G.RequiredScript
+local file = _G.file
+local mod_path = _G.ModPath
+local save_path = _G.SavePath
 
-        function self:test()
-            log("welp")
+function VojinLIB:Setup()
+    self._mod_path = mod_path
+    self._save_path = save_path
+    self._req = {}
+
+    --Now that it has different variables.. NO ONE CAN PROVE ME IT'S STOLEN FROM HOPLIB :teeth:
+    function self:run_required(path)
+        if not RequiredScript then
+            return
+        end
+
+        self._required_script = self._required_script or {}
+
+        local file_name = path .. RequiredScript:gsub(".+/(.+)", "%1.lua")
+        if self._required_script[file_name] then
+            return
+        end
+
+        if io.file_is_readable(file_name) then
+            dofile(file_name)
+        end
+
+        self._required_script[file_name] = true
+    end
+
+    function self.user_name()
+    end
+
+    local require = mod_path .. "req/"
+    for _, v in ipairs(file.GetFiles(require)) do
+        if not self._req[v] and io.file_is_readable(require .. v) then
+            dofile(require .. v)
+            self._req[v] = true --true as in HOOKED = TRUE
         end
     end
 end
---
+++ req/Hooks.lua
@@ -0,0 +1,34 @@
+if not VojinLIB then
+    return
+end
+
+VojinLIB.Hooks = {}
+local hooks = VojinLIB.Hooks
+
+---Is used as a base to add some type of an update function
+---@param type string @String, that defines which update function to hook to
+---@param id string @String to use for the hook id
+---@param clbk function @Function to call with the hook
+local function _add_update(type, id, clbk)
+    Hooks:Add(type, id, clbk)
+end
+
+---Hooks a callback with an id to an update function using bases "GameSetupUpdate" Hook
+---@param id string @String to use for the hook id
+---@param clbk function @Function to call with the hook
+function hooks.add_update(id, clbk)
+    _add_update("GameSetupUpdate", id, clbk)
+end
+
+---Hooks a callback with an id to an update function using bases "MenuUpdate" Hook
+---@param id string @String to use for the hook id
+---@param clbk function @Function to call with the hook
+function hooks.add_menu_update(id, clbk)
+    _add_update("MenuUpdate", id, clbk)
+end
+
+---Removes the specified update hook to be no longer called
+---@param id string @String to specify which hook to remove using the id
+function hooks.remove_update(id)
+    Hooks:Remove(id)
+end
