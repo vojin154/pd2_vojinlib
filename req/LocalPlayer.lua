266e1dd86240cf35aba99d4f54680a17686e0054
vojin154 <vojin154@gmail.com>
2024-05-01 00:02:36 +0200

    Add LocalPlayer (UNFINISHED)

---
+++ req/LocalPlayer.lua
@@ -0,0 +1,26 @@
+if not VojinLIB then
+    return
+end
+
+VojinLIB.LocalPlayer = {}
+local player = VojinLIB.LocalPlayer
+
+function player:is_online()
+    local is_signed_in = self:is_signed_in()
+
+    if Global.game_settings.single_player then
+        is_signed_in = false
+    end
+
+    return is_signed_in
+end
+
+function player:is_signed_in()
+    local is_signed_in = managers.network.account:signin_state() == "signed in"
+
+    if SystemInfo:matchmaking() == Idstring("MM_EPIC") and not EpicMM:logged_on() then
+        is_signed_in = false
+    end
+
+    return is_signed_in
+end
