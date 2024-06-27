534a6e028caee4bad03eae2bc2acbb2ed06fb717
vojin154 <vojin154@gmail.com>
2024-06-27 19:28:58 +0200

    s

--- req/Tables.lua
+++ req/Tables.lua
@@ -7,21 +7,13 @@ local function get_method(type)
     return table._method[type:lower()]
 end
 
-local function set_metamethod(tbl, meta_tbl)
-    local old_meta = getmetatable(tbl) or {}
-    local new_meta = meta_tbl or {}
-
-    for i, v in pairs(old_meta) do
-        new_meta[i] = v
-    end
-
-    return new_meta
-end
-
 function tables:changed(tbl, id, clbk)
     if (not tbl) or (not id) or (not clbk) then
         return
     end
+
+    local old_meta = getmetatable(tbl)
+    local meta = old_meta or {}
 
     local method = get_method("changed")
     method[id] = method[id] or {}
@@ -29,34 +21,39 @@ function tables:changed(tbl, id, clbk)
     local entry = method[id]
     local old_clbk = entry.clbk
 
-    local new_meta = set_metamethod(tbl, {})
-
-    local old_newindex = new_meta.__newindex
+    local old_newindex = meta.__newindex
 
     entry = {
-        tbl = tbl,
+        meta = meta,
         clbk = clbk or old_clbk,
         old_newindex = old_newindex
     }
 
-    entry.newindex = function(t, i, v)
-        if i == "__newindex" then
-            entry.old_newindex = v
-        elseif old_newindex then
+    meta.__newindex = function(t, i, v)
+        if old_newindex then
             old_newindex(t, i, v)
         end
 
         clbk(i, v)
     end
 
-    new_meta.__newindex = entry.newindex
+    if not old_meta then
+        setmetatable(tbl, meta)
+    end
 
-    setmetatable(tbl, new_meta)
+    setmetatable(meta, {
+        __newindex = function(t, i, v)
+            if i == "__newindex" then
+                entry.old_newindex = v
+            end
+        end
+    })
 end
 
 local function disconnect_changed(id)
     local method = get_method("changed")
     local entry = method[id]
+
     if not entry then
         return
     end
